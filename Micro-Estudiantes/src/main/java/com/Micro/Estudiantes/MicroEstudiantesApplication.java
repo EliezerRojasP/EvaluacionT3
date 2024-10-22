ce42785127c3da341bfef3c326f44d15fca187e3
EliezerRojasP <EliezerRojasPantigoso@hotmail.com>
2024-10-22 13:09:59 -0500

    EvaluacionT3

---
+++ Micro-Estudiantes/src/main/java/com/Micro/Estudiantes/MicroEstudiantesApplication.java
@@ -0,0 +1,13 @@
+package com.Micro.Estudiantes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroEstudiantesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroEstudiantesApplication.class, args);
+	}
+
+}
